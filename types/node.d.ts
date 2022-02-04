c773dba25ec519159617a39786b714075320788a
clemlak <clementlakhal@gmail.com>
2022-02-04 15:09:19 +0400

    feat: update env node types

--- types/node.d.ts
+++ types/node.d.ts
@@ -2,5 +2,9 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     TOKEN: string;
     ALCHEMY_KEY: string;
+    NETWORK_1: string;
+    NETWORK_4: string;
+    NETWORK_5: string;
+    NETWORK_42: string;
   }
 }
