d4f4f722b6992a438814d0e317a673a56a1d59ad
clemlak <clementlakhal@gmail.com>
2022-02-04 15:09:30 +0400

    feat: add multi networks support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-import { TextChannel, Client, MessageEmbed } from 'discord.js';
+import { TextChannel, Client } from 'discord.js';
 import { BigNumberish, Contract, Event, providers } from 'ethers';
 import dotenv from 'dotenv';
 import { abi as managerAbi } from '@primitivefi/rmm-manager/artifacts/contracts/PrimitiveManager.sol/PrimitiveManager.json';
 
 import { getLiquidityEmbedMessage, getCreateEmbedMessage } from './utils';
 
+import Config from './config.json';
+
 dotenv.config();
+
+interface IConfig {
+  activeNetworks: string[];
+  channels: {
+    liquidityAlerts: string,
+    swapAlerts: string,
+    testAlerts: string,
+  },
+  addresses: {
+    [key: string]: {
+      manager: string;
+      multicall: string;
+    },
+  },
+};
+
+const config = Config as IConfig;
 
 const client = new Client({
   intents: ['GUILDS', 'GUILD_MESSAGES'],
@@ -15,108 +34,115 @@ client.on('ready', async () => {
   console.log(`Logged in as ${client.user?.id}`);
   client.user?.setActivity('Primitive pools', { type: 'WATCHING' });
 
-  const provider = new providers.WebSocketProvider(
-    'wss://eth-rinkeby.alchemyapi.io/v2/55XYQVY5-bFXCcxAKkKx2-qX0jCLU4C7',
-    4,
-  );
+  for (let i = 0; i < config.activeNetworks.length; i += 1) {
+    const networkId = config.activeNetworks[i];
 
-  const manager = new Contract(
-    '0x24f2a98B9B92c5D00C746bc07cFcc4BA26956F8b',
-    managerAbi,
-    provider,
-  );
-
-  manager.on('Create', async (
-    payer: string,
-    engine: string,
-    poolId: BigNumberish,
-    strike: BigNumberish,
-    sigma: BigNumberish,
-    maturity: BigNumberish,
-    gamma: BigNumberish,
-    delLiquidity: BigNumberish,
-    event: Event,
-  ) => {
-    const embedMessage = await getCreateEmbedMessage(
-      provider,
-      '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
-      payer,
-      engine,
-      poolId,
-      delLiquidity,
-      event,
+    const provider = new providers.WebSocketProvider(
+      process.env[`NETWORK_${networkId}`]!,
+      networkId,
     );
 
-    const channel = client.channels.cache.get('934553459787718706') as TextChannel;
-
-    channel.send({
-      embeds: embedMessage.embeds,
-      files: embedMessage.files,
-    });
-  });
-
-  manager.on('Allocate', async (
-    payer: string,
-    engine: string,
-    poolId: BigNumberish,
-    delLiquidity: BigNumberish,
-    delRisky: BigNumberish,
-    delStable: BigNumberish,
-    fromMargin: boolean,
-    event: Event,
-  ) => {
-    const embedMessage = await getLiquidityEmbedMessage(
-      'Liquidity allocated',
-      'allocated into',
+    const manager = new Contract(
+      config.addresses[networkId].manager,
+      managerAbi,
       provider,
-      '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
-      payer,
-      engine,
-      poolId,
-      delLiquidity,
-      delRisky,
-      delStable,
-      event,
     );
 
-    const channel = client.channels.cache.get('934553459787718706') as TextChannel;
+    manager.on('Create', async (
+      payer: string,
+      engine: string,
+      poolId: BigNumberish,
+      strike: BigNumberish,
+      sigma: BigNumberish,
+      maturity: BigNumberish,
+      gamma: BigNumberish,
+      delLiquidity: BigNumberish,
+      event: Event,
+    ) => {
+      const embedMessage = await getCreateEmbedMessage(
+        provider,
+        config.addresses[networkId].multicall,
+        payer,
+        engine,
+        poolId,
+        delLiquidity,
+        event,
+      );
 
-    channel.send({
-      embeds: embedMessage.embeds,
-      files: embedMessage.files,
+      const channelId = networkId === "1" ? config.channels.liquidityAlerts : config.channels.testAlerts;
+      const channel = client.channels.cache.get(channelId) as TextChannel;
+
+      channel.send({
+        embeds: embedMessage.embeds,
+        files: embedMessage.files,
+      });
     });
-  });
 
-  manager.on('Remove', async (
-    payer: string,
-    engine: string,
-    poolId: BigNumberish,
-    delLiquidity: BigNumberish,
-    delRisky: BigNumberish,
-    delStable: BigNumberish,
-    event: Event,
-  ) => {
-    const embedMessage = await getLiquidityEmbedMessage(
-      'Liquidity removed',
-      'removed from',
-      provider,
-      '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
-      payer,
-      engine,
-      poolId,
-      delLiquidity,
-      delRisky,
-      delStable,
-      event,
-    );
+    manager.on('Allocate', async (
+      payer: string,
+      engine: string,
+      poolId: BigNumberish,
+      delLiquidity: BigNumberish,
+      delRisky: BigNumberish,
+      delStable: BigNumberish,
+      fromMargin: boolean,
+      event: Event,
+    ) => {
+      const embedMessage = await getLiquidityEmbedMessage(
+        'Liquidity allocated',
+        'allocated into',
+        provider,
+        config.addresses[networkId].multicall,
+        payer,
+        engine,
+        poolId,
+        delLiquidity,
+        delRisky,
+        delStable,
+        event,
+      );
 
-    const channel = client.channels.cache.get('934553459787718706') as TextChannel;
+      const channelId = networkId === "1" ? config.channels.liquidityAlerts : config.channels.testAlerts;
+      const channel = client.channels.cache.get(channelId) as TextChannel;
 
-    channel.send({
-      embeds: embedMessage.embeds,
-      files: embedMessage.files,
+      channel.send({
+        embeds: embedMessage.embeds,
+        files: embedMessage.files,
+      });
     });
-  });
+
+    manager.on('Remove', async (
+      payer: string,
+      engine: string,
+      poolId: BigNumberish,
+      delLiquidity: BigNumberish,
+      delRisky: BigNumberish,
+      delStable: BigNumberish,
+      event: Event,
+    ) => {
+      const embedMessage = await getLiquidityEmbedMessage(
+        'Liquidity removed',
+        'removed from',
+        provider,
+        config.addresses[networkId].multicall,
+        payer,
+        engine,
+        poolId,
+        delLiquidity,
+        delRisky,
+        delStable,
+        event,
+      );
+
+      const channelId = networkId === "1" ? config.channels.liquidityAlerts : config.channels.testAlerts;
+      const channel = client.channels.cache.get(channelId) as TextChannel;
+
+      channel.send({
+        embeds: embedMessage.embeds,
+        files: embedMessage.files,
+      });
+    });
+  }
 });
 
 client.on('messageCreate', async (msg) => {
